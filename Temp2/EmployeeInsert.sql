db2332d2e7740400a91c59c725518eee81bd3e58
pooja Gonla <pooja.gonla@gmail.com>
2025-05-23 11:37:50 +0530

    JIRA-123 Added some changes

--- Temp2/EmployeeInsert.sql
+++ Temp2/EmployeeInsert.sql
@@ -9,4 +9,4 @@ CREATE TABLE Employees (
 INSERT INTO Employees (id, name, age, department) VALUES
 (1, 'Alice', 30, 'HR'),
 (2, 'Bob', 28, 'IT'),
-(3, 'Charlie', 35, 'Finance');
+(3, 'CharlMarlieie', 35, 'Finance');
